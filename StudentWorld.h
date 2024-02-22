323dfadcd756bc59ae472621247ac3dcff4e8ac7
KrishJain8 <144550457+KrishJain8@users.noreply.github.com>
2024-02-22 09:50:35 -0800

    Update StudentWorld.h

--- StudentWorld.h
+++ StudentWorld.h
@@ -1,1 +1,29 @@
+#ifndef STUDENTWORLD_H_
+#define STUDENTWORLD_H_
+
+#include "GameWorld.h"
+#include "Level.h"
+#include "Actor.h"
+#include <string>
+#include <list>
+using namespace std;
+
+// Students:  Add code to this file, StudentWorld.cpp, Actor.h, and Actor.cpp
+
+class StudentWorld : public GameWorld
+{
+public:
+  StudentWorld(std::string assetPath);
+  virtual int init();
+  virtual int move();
+  virtual void cleanUp();
+
+  void addActor(Actor* actor);
+
+private:
+	list<Actor*> actors;
+	Avatar* _avatar;
+};
+
+#endif // STUDENTWORLD_H_
 
