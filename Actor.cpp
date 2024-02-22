f1a8392391d5a2fb53a78c4696f1bbfd3de1d8ad
KrishJain8 <144550457+KrishJain8@users.noreply.github.com>
2024-02-22 14:50:41 -0800

    Update Actor.cpp

--- Actor.cpp
+++ Actor.cpp
@@ -13,22 +13,34 @@ int Actor::getHealth() {
 void Actor::updateHealth(int health) {
 	_health = health;
 }
+
 StudentWorld* Actor::getStudentWorld() {
 	return _world;
 }
 
 //Avatar
 
-Avatar::Avatar(StudentWorld* world) : Actor(IID_PLAYER, 0, 0, 0, 0, world) 
+Avatar::Avatar(StudentWorld* world, double startX, double startY) : Actor(IID_PLAYER, startX, startY, 0, 0, world)
 {
 	setVisible(true);
 	updateHealth(20);
 	peaCount = 20;
 }
 void Avatar::firePea() {
+	double x = getX();
+	double y = getY();
 	//create pea at location in front of where avatar is facing
-	if (true) { //direction  == right
-
+	if (getDirection() == 90) { //direction  == right
+		return; //create pea at x + 1, y
+	}
+	if (getDirection() == 180) { //direction  == left
+		return; //create pea at x - 1, y
+	}
+	if (getDirection() == 0) { //direction  == up
+		return; //create pea at x, y + 1
+	}
+	if (getDirection() == 270) { //direction  == down
+		return; //create pea at x, y - 1
 	}
 	return;
 }
@@ -46,20 +58,28 @@ void Avatar::doSomething() {
 			updateHealth(0); //kill avatar
 			break;
 		case KEY_PRESS_UP:
-			moveTo(x, y + 1); //check for wall first
-			setDirection(90);
+			if (getStudentWorld()->runOver(x, y + 1)) {
+				moveTo(x, y + 1);
+				setDirection(90);
+			}
 			break;
 		case KEY_PRESS_DOWN:
-			moveTo(x, y - 1);
-			setDirection(270);
+			if (getStudentWorld()->runOver(x, y - 1)) {
+				moveTo(x, y - 1);
+				setDirection(270);
+			}
 			break;
 		case KEY_PRESS_LEFT:
-			moveTo(x - 1, y);
-			setDirection(180);
+			if (getStudentWorld()->runOver(x - 1, y)) {
+				moveTo(x - 1, y);
+				setDirection(180);
+			}
 			break;
 		case KEY_PRESS_RIGHT:
-			moveTo(x + 1, y);
-			setDirection(0);
+			if (getStudentWorld()->runOver(x + 1, y)) {
+				moveTo(x + 1, y);
+				setDirection(0);
+			}
 			break;
 		case KEY_PRESS_SPACE:
 			//introduce pea object
@@ -67,18 +87,23 @@ void Avatar::doSomething() {
 			peaCount--;
 			break;
 		}
-		
+
 	}
 }
-
+bool Avatar::canRunOver() {
+	return true;
+}
 
 //Wall
 
 Wall::Wall(StudentWorld* world, double startX, double startY) : Actor(IID_WALL, startX, startY, none, 1, world) {
 	setVisible(true);
+	updateHealth(1);
 }
 void Wall::doSomething() {
 	return;
 }
-
+bool Wall::canRunOver() {
+	return false;
+}
 // Students:  Add code to this file, Actor.h, StudentWorld.h, and StudentWorld.cpp
