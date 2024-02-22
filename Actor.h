cc60c6614295a227c3cb63fe3e1bd8563712f392
KrishJain8 <144550457+KrishJain8@users.noreply.github.com>
2024-02-22 14:50:59 -0800

    Update Actor.h

--- Actor.h
+++ Actor.h
@@ -9,13 +9,14 @@ class Actor : public GraphObject {
 public:
 	Actor(int imageID, double startX, double startY, int dir, double size, StudentWorld* world)
 		: GraphObject(imageID, startX, startY, dir, size) {
-			_world = world;
+		_world = world;
 	}
 	virtual void doSomething() = 0;
 	bool isAlive();
 	int getHealth();
 	void updateHealth(int health);
 	StudentWorld* getStudentWorld();
+	virtual bool canRunOver() = 0;
 
 private:
 	StudentWorld* _world;
@@ -23,18 +24,19 @@ private:
 };
 class Avatar : public Actor {
 public:
-	Avatar(StudentWorld* world);
+	Avatar(StudentWorld* world, double startX, double startY);
 	void doSomething();
 	void firePea();
+	bool canRunOver();
 private:
 	int peaCount;
-	int score;
 };
 
 class Wall : public Actor {
 public:
 	Wall(StudentWorld* world, double startX, double startY);
 	void doSomething();
+	bool canRunOver();
 private:
 
 };
